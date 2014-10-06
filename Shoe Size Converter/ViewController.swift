9e188047020af20b7cc69806515d0075f04868e7
Jordan Byrne <jordan5039@live.com>
2014-10-06 16:38:40 +0100

    removed redundant string and refactored code

--- Shoe Size Converter/ViewController.swift
+++ Shoe Size Converter/ViewController.swift
@@ -16,77 +16,77 @@ class ViewController: UIViewController {
     @IBOutlet weak var EuropeSwitch: UISwitch!
     @IBOutlet weak var convertTextField: UITextField!
     @IBOutlet weak var showConversionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func convertButton() {
         var shoeSize = convertTextField.text.toInt()!
-        var shoeSizeString: String
-        //if the user wants to convert mens shoes.
+        
+        /*
+            men's shoes
+        */
+        
         if menSwitch.on {
             //from US sizes to European sizes
             if USSwitch.on {
                 shoeSize -= 1
-                shoeSizeString = String(shoeSize)
-                showConversionLabel.text = "A US men's \(convertTextField.text) is a UK men's \(shoeSizeString)"
-                showConversionLabel.hidden = false
-                convertTextField.text = ""
+                showConversionLabel.text = "A US men's \(convertTextField.text) is a UK men's \(shoeSize)"
+                necessaryChanges()
             }
             //from European sizes to US sizes
             else {
                 shoeSize += 1
-                shoeSizeString = String(shoeSize)
-                showConversionLabel.text = "A UK men's \(convertTextField.text) is a US men's \(shoeSizeString)"
-                showConversionLabel.hidden = false
-                convertTextField.text = ""
+                showConversionLabel.text = "A UK men's \(convertTextField.text) is a US men's \(shoeSize)"
+                necessaryChanges()
             }
         }
-        //if the user wants to convert womens shoes.
+            
+        /*
+            women's shoes
+        */
+            
         else {
             //from US sizes to European sizes
             if USSwitch.on {
                 if shoeSize == 4 {
                     shoeSize -= 3
-                    shoeSizeString = String(shoeSize)
-                    showConversionLabel.text = "A US women's \(convertTextField.text) is a UK women's \(shoeSizeString)"
-                    showConversionLabel.hidden = false
-                    convertTextField.text = ""
+                    showConversionLabel.text = "A US women's \(convertTextField.text) is a UK women's \(shoeSize)"
+                    necessaryChanges()
                 }
                     
                 else {
                     shoeSize -= 4
-                    shoeSizeString = String(shoeSize)
-                    showConversionLabel.text = "A US women's \(convertTextField.text) is a UK women's \(shoeSizeString)"
-                    showConversionLabel.hidden = false
-                    convertTextField.text = ""
+                    showConversionLabel.text = "A US women's \(convertTextField.text) is a UK women's \(shoeSize)"
+                    necessaryChanges()
                 }
             }
             //from European sizes to US sizes
             else {
                 if shoeSize == 1 {
                     shoeSize += 3
-                    shoeSizeString = String(shoeSize)
-                    showConversionLabel.text = "A UK women's \(convertTextField.text) is a US women's \(shoeSizeString)"
-                    showConversionLabel.hidden = false
-                    convertTextField.text = ""
+                    showConversionLabel.text = "A UK women's \(convertTextField.text) is a US women's \(shoeSize)"
+                    necessaryChanges()
                 }
                     
                 else {
                     shoeSize += 4
-                    shoeSizeString = String(shoeSize)
-                    showConversionLabel.text = "A UK women's \(convertTextField.text) is a US women's \(shoeSizeString)"
-                    showConversionLabel.hidden = false
-                    convertTextField.text = ""
+                    showConversionLabel.text = "A UK women's \(convertTextField.text) is a US women's \(shoeSize)"
+                    necessaryChanges()
                 }
             }
         }
+    }
+    
+    func necessaryChanges() -> Void {
+        showConversionLabel.hidden = false
+        convertTextField.text = ""
+        convertTextField.resignFirstResponder()
     }
 
 }
